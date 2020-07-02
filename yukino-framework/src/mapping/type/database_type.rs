6cbaf4a76a763ed92373bc54b7439ac9a0675990
dark-flames <dark_flames@outlook.com>
2020-07-02 09:14:09 +0900

    fix: datetime feature

--- yukino-framework/src/mapping/type/database_type.rs
+++ yukino-framework/src/mapping/type/database_type.rs
@@ -1,8 +1,8 @@
-use proc_macro2::{TokenStream, Group};
-use quote::{ToTokens, quote, TokenStreamExt};
-use proc_macro2::Delimiter::Brace;
+use iroha::ToTokens;
+#[cfg(any(feature="data-time"))]
+use time::{Time, Date, PrimitiveDateTime};
 
-#[derive(Clone)]
+#[derive(Clone, ToTokens)]
 #[allow(dead_code)]
 pub enum DatabaseType {
     SmallInteger,
@@ -37,76 +37,6 @@ pub enum DatabaseType {
     Array
 }
 
-impl ToTokens for DatabaseType {
-    fn to_tokens(&self, tokens: &mut TokenStream) {
-        tokens.append(Group::new(
-            Brace,
-            match self {
-                DatabaseType::SmallInteger => quote! {
-                    yuikino::DatabaseType::SmallInteger
-                },
-                DatabaseType::UnsignedSmallInteger => quote! {
-                    yuikino::DatabaseType::UnsignedSmallInteger
-                },
-                DatabaseType::Integer => quote! {
-                    yuikino::DatabaseType::Integer
-                },
-                DatabaseType::UnsignedInteger => quote! {
-                    yuikino::DatabaseType::UnsignedInteger
-                },
-                DatabaseType::BigInteger => quote! {
-                    yuikino::DatabaseType::BigInteger
-                },
-                DatabaseType::UnsignedBigInteger => quote! {
-                    yuikino::DatabaseType::UnsignedBigInteger
-                },
-                DatabaseType::Float => quote! {
-                    yuikino::DatabaseType::Float
-                },
-                DatabaseType::Double => quote! {
-                    yuikino::DatabaseType::Double
-                },
-                DatabaseType::Decimal(p, s) => quote! {
-                    yuikino::DatabaseType::Decimal(#p, #s)
-                },
-                DatabaseType::Binary => quote! {
-                    yuikino::DatabaseType::Binary
-                },
-                #[cfg(any(feature="data-time"))]
-                DatabaseType::Time => quote! {
-                    yuikino::DatabaseType::Time
-                },
-                #[cfg(any(feature="data-time"))]
-                DatabaseType::Date => quote! {
-                    yuikino::DatabaseType::Binary
-                },
-                #[cfg(any(feature="data-time"))]
-                DatabaseType::DateTime => quote! {
-                    yuikino::DatabaseType::Binary
-                },
-                DatabaseType::Timestamp => quote! {
-                    yuikino::DatabaseType::Binary
-                },
-                DatabaseType::Character => quote! {
-                    yuikino::DatabaseType::Character
-                },
-                DatabaseType::String => quote! {
-                    yuikino::DatabaseType::String
-                },
-                DatabaseType::Text => quote! {
-                    yuikino::DatabaseType::Text
-                },
-                DatabaseType::Array => quote! {
-                    yuikino::DatabaseType::Array
-                },
-                DatabaseType::Map => quote! {
-                    yuikino::DatabaseType::Map
-                }
-            }
-        ))
-    }
-}
-
 pub type Binary = Vec<u8>;
 
 #[allow(dead_code)]
@@ -127,11 +57,11 @@ pub enum DatabaseValue {
     Binary(Binary),
 
     #[cfg(any(feature="data-time"))]
-    Time(chrono::Time),
+    Time(Time),
     #[cfg(any(feature="data-time"))]
-    Date(chrono::Date<dyn chore::TimeZone>),
+    Date(Date),
     #[cfg(any(feature="data-time"))]
-    DateTime(chrono::DateTime<dyn chore::TimeZone>),
+    DateTime(PrimitiveDateTime),
 
     Timestamp(i64),
 
