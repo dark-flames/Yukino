305b54949760b917fc9dd975e05e2e9acd97ef2f
dark-flames <dark_flames@outlook.com>
2020-07-20 15:21:25 +0900

    feat: parse for ident and value

--- yukino-core/src/query/expr/ident.rs
+++ yukino-core/src/query/expr/ident.rs
@@ -1,13 +1,48 @@
 use syn::parse::{Parse, ParseBuffer};
-use syn::Error;
+use syn::{Error, Ident, Token};
 
 #[allow(dead_code)]
+#[derive(Eq, PartialEq, Debug)]
 pub struct IdentExpression {
     segments: Vec<String>,
 }
 
 impl Parse for IdentExpression {
-    fn parse<'a>(_input: &'a ParseBuffer<'a>) -> Result<Self, Error> {
-        unimplemented!()
+    fn parse<'a>(input: &'a ParseBuffer<'a>) -> Result<Self, Error> {
+        let mut segments = vec![];
+
+        let ahead = input.lookahead1();
+
+        if ahead.peek(Ident) {
+            segments.push(
+                input.parse::<Ident>()?.to_string()
+            )
+        }
+
+        while input.peek(Token![.]) {
+            input.parse::<Token![.]>()?;
+
+            segments.push(
+                input.parse::<Ident>()?.to_string()
+            )
+        }
+
+        Ok(IdentExpression {
+            segments
+        })
     }
+}
+
+#[test]
+fn test_ident() {
+    use syn::parse_quote;
+    let ident: IdentExpression = parse_quote! {
+        a.b.c
+    };
+
+    assert_eq!(ident , IdentExpression { segments: vec![
+        "a".to_string(),
+        "b".to_string(),
+        "c".to_string()
+    ] })
 }
--- yukino-core/src/query/expr/value.rs
+++ yukino-core/src/query/expr/value.rs
@@ -1,14 +1,51 @@
 use proc_macro2::Ident;
 use syn::parse::{Parse, ParseBuffer};
-use syn::{Error, Lit};
+use syn::{Error, Lit, Token};
 
+/// ValueExpression
+/// Lit: literal like string or number ...
+/// ExternalValue: rust ident start with `@`
+#[derive(Eq, PartialEq, Debug)]
 pub enum Value {
     Lit(Lit),
     ExternalValue(Ident),
 }
 
 impl Parse for Value {
-    fn parse<'a>(_input: &'a ParseBuffer<'a>) -> Result<Self, Error> {
-        unimplemented!()
+    fn parse<'a>(input: &'a ParseBuffer<'a>) -> Result<Self, Error> {
+        let ahead = input.lookahead1();
+        if ahead.peek(Token![@]) {
+            input.parse::<Token![@]>()?;
+            Ok(Value::ExternalValue(input.parse()?))
+        } else if ahead.peek(Lit) {
+            Ok(Value::Lit(input.parse()?))
+        } else {
+            Err(ahead.error())
+        }
+    }
+}
+
+#[test]
+fn test_value() {
+    use syn::{parse_quote};
+
+    let value_lit: Value = parse_quote! {
+        "foo"
+    };
+
+    if let Value::Lit(Lit::Str(lit)) = value_lit {
+        assert_eq!(lit.value(), "foo".to_string())
+    } else {
+        panic!();
+    };
+
+    let value_external: Value = parse_quote! {
+        @bar
+    };
+
+    if let Value::ExternalValue(ident) = value_external {
+        assert_eq!(ident.to_string(), "bar".to_string())
+    } else {
+        panic!();
     }
 }
