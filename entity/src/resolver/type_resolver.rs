b3aa7eabc1c21d2a4c053cee0859c03ae3cbfcf4
dark-flames <dark_flames@outlook.com>
2021-01-20 16:05:27 +0900

    feat: finish type resolver

--- entity/src/resolver/type_resolver.rs
+++ entity/src/resolver/type_resolver.rs
@@ -1,11 +1,13 @@
 use crate::resolver::error::ResolveError;
-use syn::{ItemUse, UseTree};
+use syn::{ItemUse, UseTree, TypePath};
+use std::collections::HashMap;
+use syn::__private::ToTokens;
 
 pub type FullPath = String;
 pub type TypeName = String;
 
 pub struct TypeResolver {
-    maps: Vec<(FullPath, TypeName)>,
+    maps: HashMap<TypeName, FullPath>,
 }
 
 impl Default for TypeResolver {
@@ -16,24 +18,24 @@ impl Default for TypeResolver {
 
 impl TypeResolver {
     pub fn new() -> TypeResolver {
-        TypeResolver { maps: vec![] }
+        TypeResolver { maps: HashMap::new() }
     }
 
     pub fn append_use_item(&mut self, item: &ItemUse) -> Result<(), ResolveError> {
-        let mut result = Self::resolve_use_tree(&item.tree)?;
+        let result = Self::resolve_use_tree(&item.tree)?;
 
-        self.maps.append(&mut result);
+        self.maps.extend(result.into_iter());
 
         Ok(())
     }
 
-    fn resolve_use_tree(tree: &UseTree) -> Result<Vec<(FullPath, TypeName)>, ResolveError> {
+    fn resolve_use_tree(tree: &UseTree) -> Result<Vec<(TypeName, FullPath)>, ResolveError> {
         Ok(match tree {
             UseTree::Name(use_name) => {
                 vec![(use_name.ident.to_string(), use_name.ident.to_string())]
             }
             UseTree::Rename(use_rename) => {
-                vec![(use_rename.ident.to_string(), use_rename.rename.to_string())]
+                vec![( use_rename.rename.to_string(), use_rename.ident.to_string())]
             }
             UseTree::Path(use_path) => {
                 let current_segment = use_path.ident.to_string();
@@ -48,7 +50,7 @@ impl TypeResolver {
                 })?;
 
                 next.into_iter()
-                    .map(|(full, name)| (format!("{}::{}", current_segment, full), name))
+                    .map(|(full, name)| (name, format!("{}::{}", current_segment, full)))
                     .collect()
             }
             UseTree::Group(use_group) => use_group.items.iter().map(Self::resolve_use_tree).fold(
@@ -66,7 +68,21 @@ impl TypeResolver {
                     }
                 },
             )?,
-            UseTree::Glob(_) => return Err(ResolveError::GlobInPathIsNotSupported("*".to_string())),
+            UseTree::Glob(_) => {
+                return Err(ResolveError::GlobInPathIsNotSupported("*".to_string()))
+            }
         })
+    }
+
+    pub fn get_full_path(&self, ty: &TypePath) -> String {
+        let mut segments: Vec<_> = ty.path.segments.iter().map(
+            |segment| segment.to_token_stream().to_string()
+        ).collect();
+
+        if let Some(full) = self.maps.get(segments.first().unwrap()) {
+            segments[0] = full.clone();
+        }
+
+        segments.join("::")
     }
 }
