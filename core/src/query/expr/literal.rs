43dbdb3e5493426e6d4726369c1430dd6b02ece7
dark-flames <dark_flames@outlook.com>
2021-02-08 17:49:53 +0900

    feat: literal

--- core/src/query/expr/literal.rs
+++ core/src/query/expr/literal.rs
@@ -1,7 +1,62 @@
 use crate::query::expr::expression::TypeFlag;
 use proc_macro2::Ident;
+use syn::parse::{Parse, ParseBuffer};
+use syn::{Error, Token, Lit};
+use crate::query::expr::helper::Peekable;
 
+#[derive(Eq, PartialEq, Debug)]
 pub enum Literal {
-    Immediate { content: String, ty: TypeFlag },
+    Immediate { content: Lit, ty: TypeFlag },
     External { ident: Ident, ty: TypeFlag },
+}
+
+impl Parse for Literal {
+    fn parse<'a>(input: &'a ParseBuffer<'a>) -> Result<Self, Error> {
+        let ahead = input.lookahead1();
+        if ahead.peek(Token![@]) {
+            input.parse::<Token![@]>()?;
+            Ok(Literal::External {
+                ident: input.parse()?,
+                ty: TypeFlag::Unresolved
+            })
+        } else if ahead.peek(Lit) {
+            Ok(Literal::Immediate {
+                content: input.parse()?,
+                ty: TypeFlag::default()
+            })
+        } else {
+            Err(ahead.error())
+        }
+    }
+}
+
+impl Peekable for Literal {
+    fn peek<'a>(input: &'a ParseBuffer<'a>) -> bool {
+        input.peek(Lit) || input.peek(Token![#])
+    }
+}
+
+#[test]
+fn test_value() {
+    use syn::parse_quote;
+
+    let value_lit: Literal = parse_quote! {
+        "foo"
+    };
+
+    if let Literal::Immediate{content: Lit::Str(lit), ty: _} = value_lit {
+        assert_eq!(lit.value(), "foo".to_string())
+    } else {
+        panic!();
+    };
+
+    let value_external: Literal  = parse_quote! {
+        @bar
+    };
+
+    if let Literal::External{ident, ty: _} = value_external {
+        assert_eq!(ident.to_string(), "bar".to_string())
+    } else {
+        panic!();
+    }
 }
