3612195ad0cea43dd6921311c8cdacd4713e2dfb
dark-flames <dark_flames@outlook.com>
2021-03-17 22:05:40 +0900

    fix: select query parse

--- core/src/query/ast/select.rs
+++ core/src/query/ast/select.rs
@@ -93,55 +93,84 @@ impl FromPair for SelectQuery {
             Rule::select_query => {
                 let mut inner = pair.into_inner();
 
-                Ok(SelectQuery {
-                    select_clause: inner.next().map(SelectClause::from_pair).ok_or_else(
-                        || location.error(SyntaxError::UnexpectedPair("select_clause")),
-                    )??,
-                    from: inner.next().map(FromClause::from_pair).ok_or_else(|| {
-                        location.error(SyntaxError::UnexpectedPair("from_clause"))
-                    })??,
-                    where_clause: inner
-                        .next()
-                        .map(Expr::from_pair)
-                        .map_or(Ok(None), |v| v.map(Some))?,
-                    group_by_clause: inner
-                        .next()
-                        .map(GroupByClause::from_pair)
-                        .map_or(Ok(None), |v| v.map(Some))?,
-                    order_by_clause: inner
-                        .next()
-                        .map(OrderByClause::from_pair)
-                        .map_or(Ok(None), |v| v.map(Some))?,
-                    limit: inner
-                        .next()
-                        .map(|inner_pair| {
-                            let inner_location = Location::from(&inner_pair);
-                            match inner_pair.as_rule() {
-                                Rule::limit_clause => {
-                                    let int_pair = inner_pair.into_inner().next();
-                                    match int_pair.as_ref().map(|v| v.as_rule()) {
-                                        Some(Rule::int) => {
-                                            let content = int_pair.unwrap().as_str();
+                let select_clause =
+                    inner.next().map(SelectClause::from_pair).ok_or_else(|| {
+                        location.error(SyntaxError::UnexpectedPair("select_clause"))
+                    })??;
 
-                                            content.parse().map_err(|_| {
-                                                inner_location.error(
-                                                    SyntaxError::CannotParseIntoInteger(
-                                                        content.to_string(),
-                                                    ),
-                                                )
-                                            })
+                let from = inner
+                    .next()
+                    .map(FromClause::from_pair)
+                    .ok_or_else(|| location.error(SyntaxError::UnexpectedPair("from_clause")))??;
+
+                let current = inner.next();
+
+                let (where_clause, current) = match current.as_ref().map(|p| p.as_rule()) {
+                    Some(Rule::expr) => (
+                        current
+                            .map(Expr::from_pair)
+                            .map_or(Ok(None), |v| v.map(Some))?,
+                        inner.next(),
+                    ),
+                    _ => (None, current),
+                };
+
+                let (group_by_clause, current) = match current.as_ref().map(|p| p.as_rule()) {
+                    Some(Rule::group_by_clause) => (
+                        current
+                            .map(GroupByClause::from_pair)
+                            .map_or(Ok(None), |v| v.map(Some))?,
+                        inner.next(),
+                    ),
+                    _ => (None, current),
+                };
+
+                let (order_by_clause, current) = match current.as_ref().map(|p| p.as_rule()) {
+                    Some(Rule::order_by_clause) => (
+                        current
+                            .map(OrderByClause::from_pair)
+                            .map_or(Ok(None), |v| v.map(Some))?,
+                        inner.next(),
+                    ),
+                    _ => (None, current),
+                };
+
+                let (limit, current) = match current.as_ref().map(|p| p.as_rule()) {
+                    Some(Rule::limit_clause) => (
+                        current
+                            .map(|inner_pair| {
+                                let inner_location = Location::from(&inner_pair);
+                                match inner_pair.as_rule() {
+                                    Rule::limit_clause => {
+                                        let int_pair = inner_pair.into_inner().next();
+                                        match int_pair.as_ref().map(|v| v.as_rule()) {
+                                            Some(Rule::int) => {
+                                                let content = int_pair.unwrap().as_str();
+
+                                                content.parse().map_err(|_| {
+                                                    inner_location.error(
+                                                        SyntaxError::CannotParseIntoInteger(
+                                                            content.to_string(),
+                                                        ),
+                                                    )
+                                                })
+                                            }
+                                            _ => Err(inner_location
+                                                .error(SyntaxError::UnexpectedPair("int"))),
                                         }
-                                        _ => Err(inner_location
-                                            .error(SyntaxError::UnexpectedPair("int"))),
                                     }
+                                    _ => Err(inner_location
+                                        .error(SyntaxError::UnexpectedPair("limit_clause"))),
                                 }
-                                _ => Err(inner_location
-                                    .error(SyntaxError::UnexpectedPair("limit_clause"))),
-                            }
-                        })
-                        .map_or(Ok(None), |v| v.map(Some))?,
-                    offset: inner
-                        .next()
+                            })
+                            .map_or(Ok(None), |v| v.map(Some))?,
+                        inner.next(),
+                    ),
+                    _ => (None, current),
+                };
+
+                let offset =
+                    current
                         .map(|inner_pair| {
                             let inner_location = Location::from(&inner_pair);
                             match inner_pair.as_rule() {
@@ -167,7 +196,16 @@ impl FromPair for SelectQuery {
                                     .error(SyntaxError::UnexpectedPair("offset_clause"))),
                             }
                         })
-                        .map_or(Ok(None), |v| v.map(Some))?,
+                        .map_or(Ok(None), |v| v.map(Some))?;
+
+                Ok(SelectQuery {
+                    select_clause,
+                    from,
+                    where_clause,
+                    group_by_clause,
+                    order_by_clause,
+                    limit,
+                    offset,
                     location,
                 })
             }
@@ -269,5 +307,61 @@ fn test_select() {
             location
         },
         Rule::select_query
-    )
+    );
+
+    assert_parse_result(
+        "Select * from Test AS t WHERE t.id >= 100 Order By COUNT(t.id) DESC LIMIT 10 OFFSET 1",
+        SelectQuery {
+            select_clause: SelectClause {
+                items: vec![(
+                    Expr::ColumnIdent(ColumnIdent {
+                        segments: vec!["*".to_string()],
+                        location,
+                    }),
+                    None,
+                )],
+                location,
+            },
+            from: FromClause {
+                table: TableReference {
+                    name: "Test".to_string(),
+                    alias: Some("t".to_string()),
+                    location,
+                },
+                join: vec![],
+                location,
+            },
+            where_clause: Some(Expr::Binary(Binary {
+                operator: BinaryOperator::Bte,
+                left: Box::new(Expr::ColumnIdent(ColumnIdent {
+                    segments: vec!["t".to_string(), "id".to_string()],
+                    location,
+                })),
+                right: Box::new(Expr::Literal(Literal::Integer(Integer {
+                    value: 100,
+                    location,
+                }))),
+                location,
+            })),
+            group_by_clause: None,
+            order_by_clause: Some(OrderByClause {
+                items: vec![(
+                    Expr::FunctionCall(FunctionCall {
+                        ident: "COUNT".to_string(),
+                        parameters: vec![Expr::ColumnIdent(ColumnIdent {
+                            segments: vec!["t".to_string(), "id".to_string()],
+                            location,
+                        })],
+                        location,
+                    }),
+                    Order::Desc,
+                )],
+                location,
+            }),
+            limit: Some(10),
+            offset: Some(1),
+            location,
+        },
+        Rule::select_query,
+    );
 }
