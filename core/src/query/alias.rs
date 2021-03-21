6811b5c1fc4e5d30a77553898a755f1c7c4ebecc
dark-flames <dark_flames@outlook.com>
2021-03-21 18:59:24 +0900

    feat: expr alias collection

--- core/src/query/alias.rs
+++ core/src/query/alias.rs
@@ -1,5 +1,6 @@
-use crate::query::ast::error::SyntaxErrorWithPos;
-use crate::query::ast::TableReference;
+use crate::query::ast::error::{SyntaxError, SyntaxErrorWithPos};
+use crate::query::ast::{Expr, SelectClause, TableReference};
+use std::collections::hash_map::RandomState;
 use std::collections::HashMap;
 
 pub const GLOBAL: &str = "#global_alias";
@@ -8,10 +9,35 @@ pub trait CollectTableAlias {
     fn collect_table_alias(&self) -> Result<HashMap<String, String>, SyntaxErrorWithPos>;
 }
 
+pub trait CollectExprAlias {
+    fn collect_expr_alias(&self) -> Result<HashMap<String, Expr>, SyntaxErrorWithPos>;
+}
+
 impl CollectTableAlias for TableReference {
     fn collect_table_alias(&self) -> Result<HashMap<String, String>, SyntaxErrorWithPos> {
         let alias = self.alias.clone().unwrap_or_else(|| GLOBAL.to_string());
 
         Ok(vec![(alias, self.name.clone())].into_iter().collect())
+    }
+}
+
+#[allow(clippy::map_entry)]
+impl CollectExprAlias for SelectClause {
+    fn collect_expr_alias(&self) -> Result<HashMap<String, Expr, RandomState>, SyntaxErrorWithPos> {
+        let items = self.items.iter().filter_map(|(expr, alias_option)| {
+            alias_option.clone().map(|alias| (alias, expr.clone()))
+        });
+
+        let mut result = HashMap::new();
+
+        for (alias, expr) in items {
+            if result.contains_key(&alias) {
+                return Err(self.location.error(SyntaxError::ConflictAlias(alias)));
+            } else {
+                result.insert(alias, expr);
+            }
+        }
+
+        Ok(result)
     }
 }
